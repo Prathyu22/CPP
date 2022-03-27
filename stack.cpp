114b3281540ecaf32f21be38ce8f80ebb99f5697
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2022-03-27 18:34:24 +0530

    stack_swap()

--- stack.cpp
+++ stack.cpp
@@ -1,34 +1,39 @@
-/*
-Stacks are a type of container adapters with LIFO type of working.
-Stack uses an encapsulated object of either vector or deque (by default) or list.
-
-STACK SYNTAX :
-For creating  a stack, we must include the <stack> header file in our code. We then use this syntax to define the std::stack:
-------> template <class Type, class Container = deque<Type> > class stack;
-
-Functions associated with stack are : All are of TC = O(1)
-empty()
-size()
-top()
-push(g)
-pop()
-*/
-
+//swap() in stacks
+//syntax : stack1.swap(stack2)
 #include<iostream>
 using namespace std;
 #include<stack>
 
 int main()
 {
-    stack <int> stack;
-    stack.push(10);
-    stack.push(20);
-    stack.push(30);
-    stack.push(40);
-    while (!stack.empty())
+    stack <int> stack1;
+
+    stack <int> stack2;
+
+    stack1.push(10);
+    stack1.push(20);
+
+    stack2.push(30);
+    stack2.push(40);
+
+    stack1.swap(stack2);
+
+    cout<<"stack1: ";
+
+    while (!stack1.empty())
     {
-        cout<<" "<<stack.top(); // 40 30 20 10
-        stack.pop();
+        cout<<" "<<stack1.top(); // 40 30
+        stack1.pop();
     }
+
+    cout<<endl;
+    cout<<"stack2: ";
+
+    while (!stack2.empty())
+    {
+        cout<<" "<<stack2.top(); // 20 10
+        stack2.pop();
+    }
+
     return 0;
 }
